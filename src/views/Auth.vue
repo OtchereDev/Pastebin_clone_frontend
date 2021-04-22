d8ecf3dcea60daa14161776e36d47e8ab34724d2
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-22 15:32:17 +0000

    added a view for authentication

---
+++ src/views/Auth.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class='min-h-screen'>
+      <div class="px-5 lg:px-72 py-16 md:py-20" v-if="false">
+          <h2 class="font-medium text-2xl ">
+              Login Page
+          </h2>
+
+          <hr class="border-gray-600 border-2">
+
+          <!-- Sign In Form component goes here -->
+          <AppLogInForm/>
+
+          <!-- related pages goes here -->
+          <AuthRelatedPages/>
+      </div>
+
+      <div class="px-2 md:px-10 lg:px-72 pt-10 md:pt-20" v-else>
+          <h2 class="font-medium text-2xl ">
+              Sign Up Page
+          </h2>
+
+          <hr class="border-gray-600 border-2">
+
+          <!-- Sign Up Form component goes here -->
+          <AppSignUpForm/>
+
+          <!-- related pages goes here -->
+          <AuthRelatedPages/>
+      </div>
+      
+  </div>
+</template>
+
+<script>
+import AppLogInForm from '../components/AppLogInForm.vue'
+import AppSignUpForm from '../components/AppSignUpForm.vue'
+import AuthRelatedPages from '../components/AuthRelatedPages.vue'
+
+export default {
+    name:'Auth',
+    components:{
+        AppLogInForm,
+        AuthRelatedPages,
+        AppSignUpForm
+    }
+}
+</script>
+
+<style>
+
+</style>
