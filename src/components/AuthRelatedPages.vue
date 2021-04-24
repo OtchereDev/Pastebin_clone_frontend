9f2ed352101f75e820ec5f5477a01ec34fd880b6
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-24 14:20:08 +0000

    added components for related pages

---
+++ src/components/AuthRelatedPages.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="p-2 rounded-md border border-gray-600 w-8/12 md:w-4/12 mt-20">
+      <h3>
+          Related Pages
+      </h3>
+      <div class="flex flex-col">
+
+        <button class='bg-gray-500 rounded px-4 py-2 mt-2 font-medium'>Created New Account?</button>
+        <button class='bg-gray-500 rounded px-4 py-2 mt-2 font-medium'>Forgot Username?</button>
+        <button class='bg-gray-500 rounded px-4 py-2 mt-2 font-medium'>Forgot Password?</button>
+        <button class='bg-gray-500 rounded px-4 py-2 mt-2 font-medium'>No Activation Mail?</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
