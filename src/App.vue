edf9e78aa9310c0512489fd877df2ad26d845c4e
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-25 21:00:47 +0000

    added an app level nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
+  <TheNavBar/>
   <router-view class="bg-gray-800 text-gray-100"/>
 </template>
+
+<script>
+// @ is an alias to /src
+import TheNavBar from '@/components/TheNavBar.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    TheNavBar
+  }
+}
+</script>
 
 <style>
 
