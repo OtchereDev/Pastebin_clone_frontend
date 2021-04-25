1871f60bad5feea7ab56e45f062ddf8e26f871f4
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-25 21:08:05 +0000

    added a component for taking in email for auth stuff

---
+++ src/components/AppEmailComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <form class="w-full mt-5 md:w-10/12 md:mx-auto">
+        
+        <div class="">
+            <label class="block  mr-5" for="">Email <span class="hidden md:inline">Address</span>:<span class="text-red-500">*</span></label>
+            <input class="block w-full mt-2 p-2 bg-gray-600 rounded-lg outline-none" placeholder="Your email address" type="email" name="" id="">
+        </div>
+        <div class="mt-5 flex justify-center items-center">
+          <button class='px-4 py-2 bg-gray-700  rounded'>
+              Submit
+          </button>
+      </div>
+    </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
