54c91fe9dfed15ec3aea7a282c4ace3d2a3903b9
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-26 21:35:19 +0000

    added component for profile statics

---
+++ src/components/TheProfileStatics.vue
@@ -0,0 +1,37 @@
+<template>
+  <p>
+        Total number of active pastes: {{ActivePaste}}<br>
+        Number of public pastes: {{PublicPaste}}<br>
+        Number of private pastes: {{PrivatePaste}}<br>
+        Number of views of your Pastebin page: 10<br>
+        Number of total views of all your active pastes: {{TotalPaste}}<br>
+  </p>
+</template>
+
+<script>
+export default {
+    props:{
+        ActivePaste:{
+            type:String,
+            require: true
+        },
+        PublicPaste:{
+            type:String,
+            required: true
+        },
+        PrivatePaste:{
+            type:String,
+            required: true
+        },
+        TotalPaste:{
+            type:String,
+            required: true
+        }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
