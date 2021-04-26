f9a8e54854cb4566693d586345cdee9a95908c31
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-26 21:41:21 +0000

    added Profile icon and profile nav bar

---
+++ src/components/ProfileIcon.vue
@@ -0,0 +1,34 @@
+<template>
+    <span class="flex items-center" :class="{'mr-2':!isLast}">
+        <div class='flex justify-center items-center mr-1' >
+            <ion-icon  :name="iconName" class="text-2xl"></ion-icon>
+        </div>
+        <div :title="title">{{value}}</div>
+    </span>
+</template>
+
+<script>
+export default {
+    props:{
+        iconName:{
+            type:String,
+            required: true
+        },
+        title:{
+            type:String,
+            required: true
+        },
+        isLast:{
+            type:Boolean
+        },
+        value:{
+            type:String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TheProfileNavBar.vue
@@ -0,0 +1,41 @@
+<template>
+   <div>
+          <div class="flex ">
+              <div class='w-16 h-16 bg-gray-500 mr-3 rounded-md'></div>
+              <div class=" flex w-11/12 justify-between items-center">
+
+                <div class="">
+                    <h2 class="font-semibold text-lg mb-2">
+                        OtchereDev's Pastebin
+                    </h2>
+                    <div class='flex items-center'>
+                           
+                            <ProfileIcon iconName='eye'  value='5' title='Number of times this Pastebin page has been viewed' />
+                            <ProfileIcon iconName='eye'  value='6' title='Accumulative total unique visitors of all this users pastes' />
+                            <ProfileIcon iconName='eye'  value='165 days' title='When user joined pastebin' :isLast='true' />
+   
+                    </div>
+                </div>
+                <div>
+                    <TheSearchPaste/>
+                </div>
+              </div>
+          </div>
+      </div>
+</template>
+
+<script>
+import ProfileIcon from '@/components/ProfileIcon.vue'
+import TheSearchPaste from '@/components/TheSearchPaste.vue'
+
+export default {
+    components:{
+        ProfileIcon,
+        TheSearchPaste,
+    }
+}
+</script>
+
+<style>
+
+</style>
