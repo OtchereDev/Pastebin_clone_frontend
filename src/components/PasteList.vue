bb24a8a4a37edaed73b1759c89cc2d65df6d460b
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-26 21:36:35 +0000

    added component for current pastebins

---
+++ src/components/PasteList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="md:mt-20">
+      <h2 class="font-semibold text-xl">
+          Public Paste
+      </h2>
+      <hr class="border-gray-500">
+      <PasteListItem/>
+      <PasteListItem/>
+      <PasteListItem/>
+  </div>
+</template>
+
+<script>
+import PasteListItem from './PasteListItem.vue'
+
+export default {
+    components:{
+        PasteListItem
+    }
+}
+</script>
+
+<style>
+
+</style>
