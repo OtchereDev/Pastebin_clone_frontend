990b4592e8b4df5267c7479e4c01a86d369e861b
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-26 21:37:15 +0000

    added component of item for PasteList

---
+++ src/components/PasteListItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="text-base">
+      
+      <div class="flex">
+            <div class='flex justify-center items-center mr-1' >
+                <ion-icon  name="eye" class="text-2xl"></ion-icon>
+            </div>
+            <p class="text-blue-500">
+                Spotify DJango
+            </p>
+      </div>
+
+      <div>
+          <span>Python |</span>
+          <span> 2min ago</span>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props:{
+        title:{
+            type:String,
+            required:true
+        },
+        language:{
+            type:String,
+            required:true
+        },
+        time:{
+            type:String,
+            required:true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
