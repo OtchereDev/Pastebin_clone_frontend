c4c7275b2647c64ef57c7c7bceec48930b98f812
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-26 21:39:01 +0000

    added component for Navbar profile menu

---
+++ src/components/TheNavBarMenu.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="absolute top-full mt-2 rounded-md  w-48 right-0 bg-gray-700 overflow-hidden" @mouseleave="this.$emit('closeMenu')" :class="{hidden:!toggleMenu}">
+      
+        <TheNavBarMenuItem iconName='eye' buttonMessage='My Pastebins' />
+        <TheNavBarMenuItem iconName='eye' buttonMessage='Edit Profile' />
+        <TheNavBarMenuItem iconName='eye' buttonMessage='Edit Settings' />
+        <TheNavBarMenuItem iconName='eye' buttonMessage='Change Password' />
+        <TheNavBarMenuItem iconName='eye' buttonMessage='Logout' />
+       
+    </div>
+</template>
+
+<script>
+import TheNavBarMenuItem from '@/components/TheNavBarMenuItem'
+export default {
+    components:{
+        TheNavBarMenuItem
+    },
+    props:{
+        toggleMenu:{
+            type:Boolean
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/TheNavBarMenuItem.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex justify-between items-center p-2 cursor-pointer hover:bg-gray-500">
+        <div class='flex justify-center items-center' >
+        <ion-icon  :name="iconName" class="text-2xl"></ion-icon>
+        </div>
+        <p>
+            {{buttonMessage}}
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        iconName:{
+            type:String,
+            required:true
+        },
+        buttonMessage:{
+            type:String,
+            required:true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
