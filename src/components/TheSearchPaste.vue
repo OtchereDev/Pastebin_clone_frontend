200b42c388c1bb0337ab31959346806cd4d7759e
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-26 21:34:31 +0000

    added component for searching for user paste

---
+++ src/components/TheSearchPaste.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="w-2/12 hidden md:flex items-center">
+        <div>
+            <ion-icon name="search-outline" class="text-2xl"></ion-icon>
+        </div>
+        <div>
+            <input type="text" name="" placeholder="Search" class="p-2 bg-gray-500 rounded-md" id="">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
