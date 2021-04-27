c2ecaa9652bc829005f9bdcffbef7fdc9beb7f6c
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-27 23:25:21 +0000

    added component for paste folder name

---
+++ src/components/PasteFolderTitle.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class='flex items-center'>
+        <AppSelect :selectOption='["no folder selected"]' title='Paste Folder' />
+        <div>
+            <div class="w-8 h-8 bg-gray-50 ml-3 rounded-full flex justify-center items-center">
+                <div class='flex justify-center items-center' >
+                    <ion-icon  name="add" class="text-2xl text-blue-700 font-bold"></ion-icon>
+                   
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import AppSelect from '@/components/AppSelect.vue'
+export default {
+    components:{
+        AppSelect
+    }
+}
+</script>
+
+<style>
+
+</style>
