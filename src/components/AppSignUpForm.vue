60fef0fc6234966e93ed3446e4a97d3d103d1b6e
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-27 23:29:40 +0000

    added placeholder to form

--- src/components/AppSignUpForm.vue
+++ src/components/AppSignUpForm.vue
@@ -2,15 +2,15 @@
     <form class="w-full mt-5 md:w-10/12 md:mx-auto">
       <div class="flex  items-center justify-between">
           <label class="block  mr-5" for="">Username:<span class="text-red-500">*</span></label>
-          <input class="block w-8/12 p-2 bg-gray-600 rounded-lg outline-none" placeholder="Your username" type="text" name="" id="">
+          <input class="block w-8/12 p-2 bg-gray-600 rounded-lg outline-none" placeholder="Your username" type="text" >
       </div>
       <div class="flex  items-center justify-between mt-2">
           <label class="block  mr-5" for="">Email <span class="hidden md:inline">Address</span>:<span class="text-red-500">*</span></label>
-          <input class="block w-8/12 p-2 bg-gray-600 rounded-lg outline-none" placeholder="Your email address" type="email" name="" id="">
+          <input class="block w-8/12 p-2 bg-gray-600 rounded-lg outline-none" placeholder="Your email address" type="email" >
       </div>
       <div class="flex  items-center mt-2 justify-between">
           <label class="block  mr-5" for="">Password:<span class="text-red-500">*</span></label>
-          <input class="block w-8/12 p-2 bg-gray-600 rounded-lg outline-none" placeholder="Your password" type="password" name="" id="">
+          <input class="block w-8/12 p-2 bg-gray-600 rounded-lg outline-none" placeholder="Your password" type="password" >
       </div>
       <!-- TODO: -->
       <!-- Add a reCAPTHA to sign up process -->
