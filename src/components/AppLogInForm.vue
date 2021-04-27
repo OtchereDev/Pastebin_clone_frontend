7248247f1a80c7c8a16814a7c38eb402b287a787
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-27 23:30:18 +0000

    added placeholder to form

--- src/components/AppLogInForm.vue
+++ src/components/AppLogInForm.vue
@@ -2,11 +2,11 @@
   <form class="w-full mt-5 md:w-10/12 md:mx-auto">
       <div class="flex  items-center justify-between">
           <label class="block  mr-5" for="">Username:<span class="text-red-500">*</span></label>
-          <input class="block w-8/12 p-2 bg-gray-600 rounded-lg outline-none" placeholder="Your username" type="text" name="" id="">
+          <input class="block w-8/12 p-2 bg-gray-600 rounded-lg outline-none" placeholder="Your username" type="text" >
       </div>
       <div class="flex  items-center mt-2 justify-between">
           <label class="block  mr-5" for="">Password:<span class="text-red-500">*</span></label>
-          <input class="block w-8/12 p-2 bg-gray-600 rounded-lg outline-none" placeholder="Your password" type="password" name="" id="">
+          <input class="block w-8/12 p-2 bg-gray-600 rounded-lg outline-none" placeholder="Your password" type="password" >
       </div>
       <div class="mt-5 flex justify-center items-center">
           <button class='px-4 py-2 bg-gray-700  rounded'>
