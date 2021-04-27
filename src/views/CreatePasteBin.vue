8214fc590e6d76b84eb20d59b45c5b9cd5cccf7e
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-27 23:27:12 +0000

    added text editor and initail optional settings

--- src/views/CreatePasteBin.vue
+++ src/views/CreatePasteBin.vue
@@ -3,13 +3,26 @@
     <div class=" w-full md:w-10/12">
 
       <h2 class='font-medium text-2xl'>
-        Your Paste
+        New Paste
       </h2>
       <hr class='border-gray-500 '>
 
-      <textarea class='w-full code-area my-5 bg-gray-700 h-96 resize-none rounded-lg outline-none p-2 text-lg'></textarea>
+      <VCodeMirror @update:value='handleUpdate' 
+                    :modeOption='cmOptions' 
+                    value='' 
+                    :mode='mode'
+                    class='w-full code-area my-5 bg-gray-700 h-96 resize-none rounded-lg outline-none  text-base' />
       
+      <h2 class='font-semibold text-xl'>
+        Optional Paste Setting
+      </h2>
       <hr class='border-gray-500 '>
+      <AppSelect :selectOption='["javascript","python"]' title='Syntax highlighting' />
+      <AppSelect :selectOption='["never","1 day","1 year"]' title='Paste Expiration' />
+      <AppSelect :selectOption='["public","private"]' title='Paste Exposure' />
+      <PasteFolderTitle/>
+      
+
     </div>
 
     <div class='md:w-2/12 w-full p-2 pb-10 md:pb-0'>
@@ -21,25 +34,77 @@
 <script>
 
 import PasteList from '../components/PasteList.vue'
+import AppSelect from '../components/AppSelect.vue'
+import PasteFolderTitle from '../components/PasteFolderTitle.vue'
+import { VCodeMirror } from 'vue3-code-mirror'
+import {ref} from 'vue'
+
+
 
 export default {
     components:{    
-        PasteList
+        PasteList,
+        VCodeMirror,
+        AppSelect,
+        PasteFolderTitle
+    },
+    setup(){
+      const code= ref('const a = 10')
+      
+      const cmOptions= ref({
+        // codemirror options
+        tabSize: 4,
+        mode: 'text/python',
+        theme: 'base16-dark',
+        lineNumbers: true,
+        line: true,
+      })
+
+      const handleUpdate=(value)=>{
+        // console.log(value)
+        
+      }
+
+      const handleSwitch=()=>{
+        mode.value={
+          name:'python',json:true
+        }
+      }
+      const handleSwitchJS=()=>{
+        mode.value={
+          name:'javascript',json:true
+        }
+      }
+
+      const mode= ref({name:'javascript',json:true})
+
+      return {code,cmOptions,handleUpdate,mode,handleSwitch,handleSwitchJS}
     }
+    
 }
 </script>
 
 <style>
-  .code-area::-webkit-scrollbar{
+  .CodeMirror-vscrollbar::-webkit-scrollbar{
     width:10px;
     background: rgba(128, 128, 128, 0.644);
     border-radius:0.5rem;
 
   }
 
-  .code-area::-webkit-scrollbar-thumb{
+  .CodeMirror-vscrollbar::-webkit-scrollbar-thumb{
     width:2px;
     background:rgba(0, 0, 0, 0.678);
     border-radius:32px
+  }
+
+  .CodeMirror-vscrollbar{
+    width:10px;
+    background: rgba(128, 128, 128, 0.644);
+    border-radius:0.5rem;
+  }
+
+  .v-code-mirror{
+    border-radius: 32px;
   }
 </style>
