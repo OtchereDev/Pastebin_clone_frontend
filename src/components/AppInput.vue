e0e9f712a3c0fb62b22bfe1257fb167d3c221ff2
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-28 22:05:43 +0000

    added app level iput component

---
+++ src/components/AppInput.vue
@@ -0,0 +1,40 @@
+<template>
+  
+    <div class="flex items-center my-2 ">
+        <div  class="mr-4 w-60">
+            <h2>
+                {{title}}:<span v-if="requiredType" class="text-red-600">*</span>
+            </h2>
+        </div>
+
+        <div class="w-full">
+            <input :type="inputType" class="outline-none p-2 bg-gray-700 rounded-md w-full" >
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+export default {
+    props:{
+        title:{
+            type:String,
+            required:true
+        },
+        inputType:{
+            type:String,
+            required:true
+        },
+        requiredType:{
+            type:Boolean,
+            
+        }
+
+    }
+}
+</script>
+
+<style>
+
+</style>
