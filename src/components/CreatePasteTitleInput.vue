39a8c267d781272dc3f54c06854e71fb3c237a4a
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-28 22:17:03 +0000

    added reusable input component

---
+++ src/components/CreatePasteTitleInput.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="flex items-center my-2">
+        <div  class="mr-4 w-60">
+            <h2>
+                {{title}}:
+            </h2>
+        </div>
+        <div class="w-full">
+            <input type="text"  class="outline-none rounded-md p-2 bg-gray-700 w-full">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:{
+        title:{
+            type:String,
+            required:true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
