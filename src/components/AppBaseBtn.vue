dcc9f3095c10e11633c595258f19cbc8f47872e3
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-28 22:08:29 +0000

    added base btn component for related pages

---
+++ src/components/AppBaseBtn.vue
@@ -0,0 +1,18 @@
+<template>
+  <button   class='bg-gray-500 rounded px-4 py-2 mt-2 font-medium focus:outline-none'>{{title}}</button>
+</template>
+
+<script>
+export default {
+    props:{
+        title:{
+            type:String,
+            required:true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
