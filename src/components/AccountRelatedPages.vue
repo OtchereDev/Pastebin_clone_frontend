8eb3026c4771344ec10c8b1019eb740a329b48d1
Oliver Otchere <oliverotchere4@gmail.com>
2021-04-28 22:14:05 +0000

    added related pages for account related pages

---
+++ src/components/AccountRelatedPages.vue
@@ -0,0 +1,29 @@
+<template>
+   <div class="p-2 rounded-md border border-gray-600 w-8/12 md:w-4/12 mt-20">
+      <h3>
+          Account Related Pages
+      </h3>
+      <div class="flex flex-col">
+
+        <AppBaseBtn title='Profile' />
+        <AppBaseBtn title='Settings' />
+        <AppBaseBtn title='Password Change' />
+        <AppBaseBtn title='Delete Account' />
+       
+
+      </div>
+  </div>
+</template>
+
+<script>
+import AppBaseBtn from '@/components/AppBaseBtn.vue'
+export default {
+  components:{
+    AppBaseBtn
+  }
+}
+</script>
+
+<style>
+
+</style>
